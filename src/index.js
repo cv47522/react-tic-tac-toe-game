8bdcfaf5cc819193f9c1666ecda42f7f3b15aa6b
Wendy <cv47522@gmail.com>
2020-01-21 15:03:38 +0200

    New Taking Turns feature: Add xIsNext state property to Board

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import './index.css';
 
 function Square(props) {
   return (
-  <button className="square" onClick={props.onClick}>)
+  <button className="square" onClick={props.onClick}>
     {props.value}
   </button>
   );
@@ -28,14 +28,19 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
-    // .slice() creates a copy of the squares array to modify instead of modifying the existing array.
+    // .slice() creates a copy of the squares array to modify
+    // instead of modifying the existing array.
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -48,7 +53,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
