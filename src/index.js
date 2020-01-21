6ebc71c1caaf49a9d51f75cab8ae34745d22dadb
Wendy <cv47522@gmail.com>
2020-01-21 16:04:10 +0200

    Add jumpTo method and key in Game to update that stepNumber

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ class Board extends React.Component {
     return (
         <Square
           value={this.props.squares[i]}
-          onClick={() => this.props.handleClick(i)}
+          onClick={() => this.props.onClick(i)}
         />
     );
   }
@@ -60,41 +60,51 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
+      history: [
+        {
         squares: Array(9).fill(null),
-      }],
+        }
+      ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     // .slice() creates a copy of the squares array to modify
     // instead of modifying the existing array.
-    const squares = this.state.squares.slice();
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
       return (
-        <li>
+        <li key={ move }>
           <button onClick={() => this.jumpTo(move)}>{ desc }</button>
         </li>
       );
